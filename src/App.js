b46105ebbc9cef6a0ce10b4852b59d2c9db2c9fb
Mohankumar@LAPTOP-DSRRQ3HB <gmk9155@gmail.com>
2023-08-26 16:59:45 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ const App = () => {
                             return {...card, matched:true}
                         }
                         else{
-                            return {...card, matched:false};
+                            return {...card};
                         }
                     })
                 })
